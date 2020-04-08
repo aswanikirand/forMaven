b2a93ba4b2056d852e9852a6762c661478d576ad
Windows10 <Windows10@DESKTOP-VQ2T56M>
2020-04-08 21:28:58 +0530

    asfsfdsa

--- forMaven/src/test/java/forMaven/OpenBrowser.java
+++ forMaven/src/test/java/forMaven/OpenBrowser.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenBrowser {
 
 	public static void main(String[] args) {
-		WebDriver driver= new ChromeDriver();
-		driver.get("https://google.com");
+		System.out.println("my Git Project");
 
 	}
 
