17fbb8ef22b72d31fc6f8c2d6f808ba2abd48f83
aswanikirand <aswanikirand@gmail.com>
2020-04-09 16:49:46 +0530

    maven test

--- forMaven/src/test/java/forMaven/test1.java
+++ forMaven/src/test/java/forMaven/test1.java
@@ -2,10 +2,12 @@ package forMaven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class OpenBrowser {
+public class test1 {
 
-	public static void main(String[] args) {
+	@Test
+	public void main1() {
 		WebDriver driver =  new ChromeDriver();
 		driver.get("https://google.com");
 
