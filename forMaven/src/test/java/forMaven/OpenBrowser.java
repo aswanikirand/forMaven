5ddcd01f04e19dc7bb0ee1279825b8650730c6cc
aswanikirand <aswanikirand@gmail.com>
2020-04-09 12:47:53 +0530

    fdasfasf

--- forMaven/src/test/java/forMaven/OpenBrowser.java
+++ forMaven/src/test/java/forMaven/OpenBrowser.java
@@ -1,12 +1,13 @@
 package forMaven;
 
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenBrowser {
 
 	public static void main(String[] args) {
-		System.out.println("my Git Project");
+		WebDriver driver =  new ChromeDriver();
+		driver.get("https://google.com");
 
 	}
 
